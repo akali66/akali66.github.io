06d999c2d1ce8f74af31ab5270a06e5e9deaafbd
akali66 <1628812212@qq.com>
2026-04-10 16:05:39 +0800

    feat: :fire: 主题切换功能+推送github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://akali66.github.io',
+  base: 'akali66.github.io',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
